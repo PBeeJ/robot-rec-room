1c536779ef7adc3649749c85c19df012dc198be4
pkibbey <phineas.kibbey@gmail.com>
2021-09-19 22:53:58 -0700

    Feature/add routes (#5)
    * add react native router package
    
    * Added home route to select robot
    
    * styling tweaks and comments
    
    * save initial options to storage
    
    * missed a file
    
    * clean up
    
    * Robot will attempt to connect to ip address and fail if the connection is not there
    
    Co-authored-by: Phineas Kibbey <pkibbey@liveperson.com>

--- App.js
+++ App.js
@@ -1,33 +1,39 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useRef, useState} from 'react';
-import {Pressable, StyleSheet} from 'react-native';
-
+import {StyleSheet, View} from 'react-native';
+import {NativeRouter, Route, Link} from 'react-router-native';
 import WS from 'react-native-websocket';
 
-import Loading from './components/Loading.js';
 import Controls from './components/Controls.js';
-// import Video from './components/Video.js';
+import Home from './components/Home.js';
+import {Home as HomeIcon, Sliders as ControlsIcon} from 'react-native-feather';
+import storage from './storage.js';
 
 import {
-  // CAMERA_URL,
+  ICON_SIZE,
+  BUTTON_SIZE,
   OFFLINE_MODE,
-  WEBSOCKET_URL, // Ooh, look at me.
+  WEBSOCKET_PORT,
   SERVER_INFO_UPDATE_SPEED,
 } from '@env';
 
 const isOnline = OFFLINE_MODE !== 'true';
-
+const INITIAL_OPTIONS = [
+  {name: 'Phineas', address: '192.168.1.10'},
+  {name: 'Passable', address: '10.1.1.10'},
+];
 export default function App() {
   const socketRef = useRef();
   const [information, setInformation] = useState(null);
+  const [IPAddress, setIPAddress] = useState(null);
+  const [options, setOptions] = useState([]);
 
   const [lastCommand, setLastCommand] = useState(null);
-  const [isLoading, setIsLoading] = useState(isOnline && true); // Don't show loading when offline
 
   function sendMessage(message) {
     if (socketRef?.current?.send) {
       socketRef?.current?.send(message);
       if (message !== 'get_info') {
-        console.log('message: ', message);
         setLastCommand(message);
       }
     }
@@ -36,7 +42,6 @@ export default function App() {
   function handleMessage({data}) {
     // Don't parse the string if it contains the initial congratulation message
     if (typeof data === 'string' && !data.includes('congratulation')) {
-      setIsLoading(false);
       try {
         const message = JSON.parse(data);
         if (message.title === 'get_info') {
@@ -52,48 +57,123 @@ export default function App() {
     sendMessage('admin:123456'); // Authorize the connection
   }
 
+  function handleError({message}) {
+    console.log('handleError:', message);
+    setIPAddress(null);
+  }
+
+  function handleClose() {
+    console.log('Connection closed');
+  }
+
+  // Fetch initial options from storage
   useEffect(() => {
-    let timer;
-    // Fetch the info data
-    clearInterval(timer);
-    timer = setInterval(() => {
-      sendMessage('get_info');
-    }, parseInt(SERVER_INFO_UPDATE_SPEED, 10));
-    return () => clearInterval(timer);
+    storage
+      .load({key: 'options', autoSync: true, syncInBackground: true})
+      .then(initialOptions =>
+        setOptions(initialOptions.length ? initialOptions : INITIAL_OPTIONS),
+      )
+      .catch(err => console.warn(err.message));
   }, []);
 
+  // Persist options to storage
+  useEffect(() => {
+    if (options.length) {
+      storage
+        .save({key: 'options', data: options})
+        .catch(err => console.warn(err.message));
+    }
+  }, [options]);
+
+  useEffect(() => {
+    if (IPAddress) {
+      let timer;
+      // Fetch the info data
+      clearInterval(timer);
+      timer = setInterval(() => {
+        sendMessage('get_info');
+      }, parseInt(SERVER_INFO_UPDATE_SPEED, 10));
+      return () => clearInterval(timer);
+    }
+  }, [IPAddress]);
+
   return (
-    <Pressable style={styles.container}>
-      {isOnline && (
+    <View style={styles.container}>
+      {isOnline && IPAddress !== null && (
         <WS
           ref={socketRef}
-          url={`ws://${WEBSOCKET_URL}`}
+          url={`ws://${IPAddress}:${WEBSOCKET_PORT}`}
           onOpen={handleOpen}
           onMessage={handleMessage}
-          onError={console.log}
-          onClose={console.log}
+          onError={handleError}
+          onClose={handleClose}
           reconnect
         />
       )}
-      {isLoading ? (
-        <Loading message={`Connecting to ${WEBSOCKET_URL}`} />
-      ) : (
-        <>
+      <NativeRouter>
+        <View style={styles.nav}>
+          <Link to="/" style={{...styles.navItem, borderColor: 'orange'}}>
+            <HomeIcon
+              stroke="orange"
+              fill="none"
+              width={parseInt(ICON_SIZE, 10)}
+              height={parseInt(ICON_SIZE, 10)}
+              strokeWidth={1.5}
+            />
+          </Link>
+          <Link
+            to="/controls"
+            style={{...styles.navItem, borderColor: 'lightgreen'}}>
+            <ControlsIcon
+              stroke="lightgreen"
+              width={parseInt(ICON_SIZE, 10)}
+              height={parseInt(ICON_SIZE, 10)}
+              strokeWidth={1.5}
+            />
+          </Link>
+        </View>
+        <Route exact path="/">
+          <Home
+            setIPAddress={setIPAddress}
+            IPAddress={IPAddress}
+            options={options}
+            setOptions={setOptions}
+          />
+        </Route>
+        <Route path="/controls">
           <Controls
             information={information}
             lastCommand={lastCommand}
             sendMessage={sendMessage}
           />
-          {/* <Video url={`http://${CAMERA_URL}/video_feed`} /> */}
-        </>
-      )}
-    </Pressable>
+        </Route>
+      </NativeRouter>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#888',
+    backgroundColor: '#333',
+    alignItems: 'center',
+  },
+  nav: {
+    position: 'absolute',
+    top: 10,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    zIndex: 2,
+  },
+  navItem: {
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 100,
+    borderWidth: 1,
+    width: parseInt(BUTTON_SIZE, 10),
+    height: parseInt(BUTTON_SIZE, 10),
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 4,
   },
 });
---
+++ components/Home.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import {useState} from 'react/cjs/react.development';
+import {PlusCircle} from 'react-native-feather';
+
+// TODO: save ip address array to phone storage
+
+// TOOD: allow people to clear their storage or delete individual items
+
+// TODO: wait for socket to reconnect before allowing access to controls route
+
+export default function Home({IPAddress, setIPAddress, options, setOptions}) {
+  const [newAddress, setNewAddress] = useState('');
+  const [newName, setNewName] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Choose a robot:</Text>
+      <Picker
+        selectedValue={IPAddress}
+        onValueChange={value => setIPAddress(value)}
+        style={styles.picker}
+        mode="dropdown">
+        <Picker.Item
+          key={'null'}
+          label={'Select a robot'}
+          value={null}
+          style={styles.pickerItem}
+        />
+        {options.map(({name, address}) => {
+          return (
+            <Picker.Item
+              key={`${name} (${address})`}
+              label={`${name} (${address})`}
+              value={address}
+              style={styles.pickerItem}
+            />
+          );
+        })}
+      </Picker>
+      <Text style={styles.title}>Add a new robot:</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          onChangeText={setNewName}
+          value={newName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="IP Address"
+          onChangeText={setNewAddress}
+          value={newAddress}
+        />
+        <Pressable
+          android_ripple={{
+            color: 'rgba(0,0,0,0.2)',
+            borderless: true,
+          }}
+          onPress={() => {
+            setOptions(opt => [...opt, {name: newName, address: newAddress}]);
+            setNewAddress('');
+            setNewName('');
+            setIPAddress(newAddress);
+          }}>
+          <PlusCircle
+            stroke={'white'}
+            width={30}
+            height={30}
+            style={styles.icon}
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picker: {
+    width: 300,
+    color: 'white',
+    backgroundColor: 'rgb(0,80,0)',
+  },
+  pickerItem: {
+    color: 'rgba(255,255,255,0.8)',
+    backgroundColor: 'darkgreen',
+  },
+  title: {
+    color: 'white',
+    marginBottom: 10,
+    marginTop: 30,
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    width: 160,
+    height: 40,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    borderRadius: 2,
+    padding: 10,
+  },
+});
